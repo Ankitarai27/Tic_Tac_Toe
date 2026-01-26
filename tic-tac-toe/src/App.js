1159bd2240bb6fc0e26ffe25a796500b7e1866bb
Ankita Rai <129681815+Ankitarai27@users.noreply.github.com>
2026-01-26 17:38:24 +0000

    #updated react

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';  
+import './App.css';
+import Boards from './TicTacToe/Board';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Boards/>
+    </div>
+  );
+}
+
+export default App;
